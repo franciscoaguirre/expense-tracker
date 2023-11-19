149251282fa3a46bb304ea5ef99dad717146b50a
Francisco Aguirre <franciscoaguirreperez@gmail.com>
2023-11-19 09:47:29 -0300

    Hook pie chart to expense categories

--- lib/pie_chart.dart
+++ lib/pie_chart.dart
@@ -1,7 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:fl_chart/fl_chart.dart';
+import 'dart:math';
 
 import 'expense.dart';
+
+Color getRandomColor() {
+  Random random = Random();
+  // Generate a random RGB color
+  int r = random.nextInt(256); // Red value between 0 and 255
+  int g = random.nextInt(256); // Green value between 0 and 255
+  int b = random.nextInt(256); // Blue value between 0 and 255
+  return Color.fromRGBO(r, g, b, 1); // Alpha is set to 1 for full opacity
+}
 
 class PieChartView extends StatelessWidget {
   final List<Expense> expenses;
@@ -19,20 +29,24 @@ class PieChartView extends StatelessWidget {
       return const Center(child: CircularProgressIndicator());
     }
 
+    final Map<String, double> amountPerCategory = {};
+    for (var expense in expenses) {
+      amountPerCategory[expense.category] =
+          (amountPerCategory[expense.category] ?? 0.0) + expense.amount;
+    }
+
     return PieChart(
       PieChartData(
-        sections: [
-          PieChartSectionData(
-            value: 40,
-            color: Colors.blue,
-            title: '40%',
-          ),
-          PieChartSectionData(
-            value: 60,
-            color: Colors.red,
-            title: '60%',
-          ),
-        ],
+        sections: amountPerCategory
+            .map((category, amount) => MapEntry(
+                category,
+                PieChartSectionData(
+                  value: amount,
+                  title: category,
+                  color: getRandomColor(),
+                )))
+            .values
+            .toList(),
       ),
     );
   }
